2493e4c5daaa0048c46d31507c67b29d6ddf7a33
Godi Blasio <100287431+BlasioGodi@users.noreply.github.com>
2024-06-09 15:43:16 +0300

    Subscribe page form modifications

---
+++ FinanceKnowHow/FinanceKnowHow/Models/SubscribeForm.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceKnowHow.Models
+{
+    public class SubscribeForm
+    {
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? FirstName { get; set; }
+        [Required]
+        public string? Surname { get; set;}
+        [Required]
+        public List<string>? City { get; set; }
+        [Required]
+        public List<string>? Country { get; set; }
+
+    }
+}
